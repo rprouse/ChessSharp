b5f8b6518ad7b8d1eac2343a02a391919e3b0512
Rob Prouse <rob@prouse.org>
2025-07-06 11:16:05 -0400

    Rook and queen check on same file tests

--- Chess.Engine.Test/CheckTests.cs
+++ Chess.Engine.Test/CheckTests.cs
@@ -2,7 +2,7 @@ namespace Chess.Engine.Test;
 
 public class CheckTests
 {
-    // Rook on same rank
+    // Rook on same rank black
     [TestCase("8/Rk6/8/8/8/8/6K1/8 b - - 0 1", PieceColour.Black, true)]
     [TestCase("8/1kR5/8/8/8/8/6K1/8 b - - 0 1", PieceColour.Black, true)]
     [TestCase("8/1k1R4/8/8/8/8/6K1/8 b - - 0 1", PieceColour.Black, true)]
@@ -11,7 +11,7 @@ public class CheckTests
     [TestCase("8/1k4R1/8/8/8/8/6K1/8 b - - 0 1", PieceColour.Black, true)]
     [TestCase("8/1k5R/8/8/8/8/6K1/8 b - - 0 1", PieceColour.Black, true)]
 
-    // Queen on same rank
+    // Queen on same rank black
     [TestCase("8/Qk6/8/8/8/8/6K1/8 b - - 0 1", PieceColour.Black, true)]
     [TestCase("8/1kQ5/8/8/8/8/6K1/8 b - - 0 1", PieceColour.Black, true)]
     [TestCase("8/1k1Q4/8/8/8/8/6K1/8 b - - 0 1", PieceColour.Black, true)]
@@ -20,7 +20,7 @@ public class CheckTests
     [TestCase("8/1k4Q1/8/8/8/8/6K1/8 b - - 0 1", PieceColour.Black, true)]
     [TestCase("8/1k5Q/8/8/8/8/6K1/8 b - - 0 1", PieceColour.Black, true)]
 
-    // Rook on same rank
+    // Rook on same rank white
     [TestCase("8/rK6/8/8/8/8/6k1/8 b - - 0 1", PieceColour.White, true)]
     [TestCase("8/1Kr5/8/8/8/8/6k1/8 b - - 0 1", PieceColour.White, true)]
     [TestCase("8/1K1r4/8/8/8/8/6k1/8 b - - 0 1", PieceColour.White, true)]
@@ -29,7 +29,7 @@ public class CheckTests
     [TestCase("8/1K4r1/8/8/8/8/6k1/8 b - - 0 1", PieceColour.White, true)]
     [TestCase("8/1K5r/8/8/8/8/6k1/8 b - - 0 1", PieceColour.White, true)]
 
-    // Queen on same rank
+    // Queen on same rank white
     [TestCase("8/qK6/8/8/8/8/6k1/8 b - - 0 1", PieceColour.White, true)]
     [TestCase("8/1Kq5/8/8/8/8/6k1/8 b - - 0 1", PieceColour.White, true)]
     [TestCase("8/1K1q4/8/8/8/8/6k1/8 b - - 0 1", PieceColour.White, true)]
@@ -38,7 +38,7 @@ public class CheckTests
     [TestCase("8/1K4q1/8/8/8/8/6k1/8 b - - 0 1", PieceColour.White, true)]
     [TestCase("8/1K5q/8/8/8/8/6k1/8 b - - 0 1", PieceColour.White, true)]
 
-    // Blocked Rook on same rank
+    // Blocked Rook on same rank black
     [TestCase("8/Rpk5/8/8/8/8/6K1/8 b - - 0 1", PieceColour.Black, false)]
     [TestCase("8/1kpR4/8/8/8/8/6K1/8 b - - 0 1", PieceColour.Black, false)]
     [TestCase("8/1kp1R3/8/8/8/8/6K1/8 b - - 0 1", PieceColour.Black, false)]
@@ -46,7 +46,7 @@ public class CheckTests
     [TestCase("8/1kp3R1/8/8/8/8/6K1/8 b - - 0 1", PieceColour.Black, false)]
     [TestCase("8/1kp4R/8/8/8/8/6K1/8 b - - 0 1", PieceColour.Black, false)]
 
-    // Blocked Queen on same rank
+    // Blocked Queen on same rank black
     [TestCase("8/Qpk5/8/8/8/8/6K1/8 b - - 0 1", PieceColour.Black, false)]
     [TestCase("8/1kpQ4/8/8/8/8/6K1/8 b - - 0 1", PieceColour.Black, false)]
     [TestCase("8/1kp1Q3/8/8/8/8/6K1/8 b - - 0 1", PieceColour.Black, false)]
@@ -54,7 +54,7 @@ public class CheckTests
     [TestCase("8/1kp3Q1/8/8/8/8/6K1/8 b - - 0 1", PieceColour.Black, false)]
     [TestCase("8/1kp4Q/8/8/8/8/6K1/8 b - - 0 1", PieceColour.Black, false)]
 
-    // Blocked Rook on same rank
+    // Blocked Rook on same rank white
     [TestCase("8/rpK5/8/8/8/8/6k1/8 b - - 0 1", PieceColour.White, false)]
     [TestCase("8/1Kpr4/8/8/8/8/6k1/8 b - - 0 1", PieceColour.White, false)]
     [TestCase("8/1Kp1r3/8/8/8/8/6k1/8 b - - 0 1", PieceColour.White, false)]
@@ -62,13 +62,38 @@ public class CheckTests
     [TestCase("8/1Kp3r1/8/8/8/8/6k1/8 b - - 0 1", PieceColour.White, false)]
     [TestCase("8/1Kp4r/8/8/8/8/6k1/8 b - - 0 1", PieceColour.White, false)]
 
-    // Blocked Queen on same rank
+    // Blocked Queen on same rank white
     [TestCase("8/qpK5/8/8/8/8/6k1/8 b - - 0 1", PieceColour.White, false)]
     [TestCase("8/1Kpq4/8/8/8/8/6k1/8 b - - 0 1", PieceColour.White, false)]
     [TestCase("8/1Kp1q3/8/8/8/8/6k1/8 b - - 0 1", PieceColour.White, false)]
     [TestCase("8/1Kp2q2/8/8/8/8/6k1/8 b - - 0 1", PieceColour.White, false)]
     [TestCase("8/1Kp3q1/8/8/8/8/6k1/8 b - - 0 1", PieceColour.White, false)]
     [TestCase("8/1Kp4q/8/8/8/8/6k1/8 b - - 0 1", PieceColour.White, false)]
+
+    // Rook on same file
+    [TestCase("1R6/1k6/8/8/8/8/6K1/8 b - - 0 1", PieceColour.Black, true)]
+    [TestCase("8/1k6/8/8/8/8/6K1/1R6 b - - 0 1", PieceColour.Black, true)]
+    [TestCase("1r6/1K6/8/8/8/8/6k1/8 b - - 0 1", PieceColour.White, true)]
+    [TestCase("8/1K6/8/8/8/8/6k1/1r6 b - - 0 1", PieceColour.White, true)]
+
+    // Queen on same file
+    [TestCase("1Q6/1k6/8/8/8/8/6K1/8 b - - 0 1", PieceColour.Black, true)]
+    [TestCase("8/1k6/8/8/8/8/6K1/1Q6 b - - 0 1", PieceColour.Black, true)]
+    [TestCase("1q6/1K6/8/8/8/8/6k1/8 b - - 0 1", PieceColour.White, true)]
+    [TestCase("8/1K6/8/8/8/8/6k1/1q6 b - - 0 1", PieceColour.White, true)]
+
+    // Blocked Rook on same file
+    [TestCase("1R6/1p6/1k6/8/8/8/6K1/8 b - - 0 1", PieceColour.Black, false)]
+    [TestCase("8/8/1k6/1p6/8/8/6K1/1R6 b - - 0 1", PieceColour.Black, false)]
+    [TestCase("1r6/1P6/1K6/8/8/8/6k1/8 b - - 0 1", PieceColour.White, false)]
+    [TestCase("8/8/1K6/1P6/8/8/6k1/1r6 b - - 0 1", PieceColour.White, false)]
+
+    // Blocked Queen on same file
+    [TestCase("1Q6/1p6/1k6/8/8/8/6K1/8 b - - 0 1", PieceColour.Black, false)]
+    [TestCase("8/8/1k6/1p6/8/8/6K1/1Q6 b - - 0 1", PieceColour.Black, false)]
+    [TestCase("1q6/1P6/1K6/8/8/8/6k1/8 b - - 0 1", PieceColour.White, false)]
+    [TestCase("8/8/1K6/1P6/8/8/6k1/1q6 b - - 0 1", PieceColour.White, false)]
+
     public void CanDetermineCheck(string fen, PieceColour colour, bool expected)
     {
         var board = BoardFactory.Create(fen);
